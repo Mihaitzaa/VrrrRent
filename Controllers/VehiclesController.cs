0293da4c3e609e9ebe8832238e666631e4c33540
Mihaitzaa <oprea.mihai.x7n@student.ucv.ro>
2022-03-24 23:29:04 +0200

    Initial commit

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,148 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VrrrRent.Models;
+using VrrrRent.Services;
+
+namespace VrrrRent.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        private readonly VehicleService _vehicleService;
+
+        public VehiclesController(VehicleService vehicleService)
+        {
+            _vehicleService = vehicleService;
+        }
+
+        // GET: Vehicles
+        public IActionResult Index()
+        {
+            var vehicles = _vehicleService.GetVehicles();
+            return View(vehicles);
+        }
+
+        // GET: Vehicles/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicle = _vehicleService.GetVehicles().FirstOrDefault(m => m.ID == id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicle);
+        }
+
+        // GET: Vehicles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Vehicles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("ID,Model,Class,Year,Brand,Available")] Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                _vehicleService.AddVehicle(vehicle);
+                _vehicleService.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicle);
+        }
+
+        // GET: Vehicles/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicle = _vehicleService.GetVehicles().FirstOrDefault(m => m.ID == id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+            return View(vehicle);
+        }
+
+        // POST: Vehicles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("ID,Model,Class,Year,Brand,Available")] Vehicle vehicle)
+        {
+            if (id != vehicle.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _vehicleService.UpdateVehicle(vehicle);
+                    _vehicleService.Save();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VehicleExists(vehicle.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicle);
+        }
+
+        // GET: Vehicles/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicle = _vehicleService.GetVehicles().FirstOrDefault(m => m.ID == id);  
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicle);
+        }
+
+        // POST: Vehicles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var vehicle = _vehicleService.GetVehiclesByCondition(m => m.ID==id).FirstOrDefault();
+            _vehicleService.DeleteVehicle(vehicle);
+            _vehicleService.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VehicleExists(int id)
+        {
+            return _vehicleService.GetVehicles().Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,91 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VrrrRent.Abstractions;
+using VrrrRent.Repositories;
+using VrrrRent.Services;
+using VrrrRent.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace VrrrRent
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<VrrrRentContext>();
+
+            services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+            services.AddScoped<ClientService>();
+            services.AddScoped<DealershipService>();
+            services.AddScoped<InventoryService>();
+            services.AddScoped<PaymentService>();
+            services.AddScoped<RentalService>();
+            services.AddScoped<VehicleService>();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+
+                options.LoginPath = "/Identity/Account/Login";
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=VrrrRentContext-1;Trusted_connection=True;ConnectRetryCount=0";
+            services.AddDbContext<VrrrRentContext>
+                (options => options.UseSqlServer(connection));
+           
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
